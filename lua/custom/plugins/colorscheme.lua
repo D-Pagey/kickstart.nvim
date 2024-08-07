638432ce68f99667c934362b6f6ce02e321e614e
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-08-07 21:29:56 +0100

    colorscheme to catpuccin

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
-  'ellisonleao/gruvbox.nvim',
+  'catppuccin/nvim',
+  name = 'catppuccin',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
-    vim.cmd.colorscheme 'gruvbox'
+    vim.cmd.colorscheme 'catppuccin-macchiato'
 
     -- You can configure highlights by doing something like:
     -- vim.cmd.hi 'Comment gui=none'
