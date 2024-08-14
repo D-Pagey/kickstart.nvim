b23bc4ab720903239e2c306520121be48bce6503
Dan Page <26827986+D-Pagey@users.noreply.github.com>
2024-08-14 11:30:14 +0100

    fix whichkey config

--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -1,22 +1,24 @@
-return { -- Useful plugin to show you pending keybinds.
+return {
   'folke/which-key.nvim',
-  event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
-
-    -- Document existing key chains
-    require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-      ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-    }
-    -- visual mode
-    require('which-key').register({
-      ['<leader>h'] = { 'Git [H]unk' },
-    }, { mode = 'v' })
-  end,
+  event = 'VeryLazy',
+  opts = {
+    spec = {
+      { '<leader>s', desc = '[S]earch' },
+      { '<leader>c', desc = '[C]ode' },
+      { '<leader>d', desc = '[D]ocument' },
+      { '<leader>r', desc = '[R]ename' },
+      { '<leader>w', desc = '[W]orkspace' },
+      { '<leader>t', desc = '[T]oggle' },
+      { '<leader>h', desc = 'Git [H]unk' },
+    },
+  },
+  keys = {
+    {
+      '<leader>?',
+      function()
+        require('which-key').show { global = false }
+      end,
+      desc = 'Buffer Local Keymaps (which-key)',
+    },
+  },
 }
